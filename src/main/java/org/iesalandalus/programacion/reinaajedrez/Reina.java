d3be835a9d6f6deabc64ccfe4c3a4453bc3287aa
isma <isma@WINDELL-BI0390A>
2022-11-14 19:28:43 +0100

    Creación de los métodos get y set.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
@@ -4,5 +4,24 @@ public class Reina {
 	private Color color;
 	private Posicion posicion;
 	
-	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if(color == null) {
+			if(posicion==null) {
+				throw new NullPointerException("EL color no debe ser nulo");
+			}
+		}
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion){
+		if(posicion==null) {
+			throw new NullPointerException("La posicion no debe ser nula");
+		}
+		this.posicion = posicion;
+	}
 }
