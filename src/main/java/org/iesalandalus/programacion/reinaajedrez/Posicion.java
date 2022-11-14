b66f57d498a447b447a2f2048b2fded809fe0492
isma <isma@WINDELL-BI0390A>
2022-11-14 19:09:34 +0100

    Creación de los métodos equals y hashCode.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 	//ATRIBUTOS
 	private int fila;
@@ -39,5 +41,22 @@ public class Posicion {
 		setFila(posicion.getFila());
 		setColumna(posicion.getColumna());
 	}
+	//////////////////////////////////////////////////////////////////////////
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Posicion))
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	
+	
+	
 	
 }
