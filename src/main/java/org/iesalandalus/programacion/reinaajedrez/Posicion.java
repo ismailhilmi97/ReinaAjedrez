aab823f5955cff7606a501f942218eafaf27596b
isma <isma@WINDELL-BI0390A>
2022-11-14 18:49:09 +0100

    Creación de los métodos get y set para los atributos y comprobar que los valores indicados en set sean correctos.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -4,4 +4,29 @@ public class Posicion {
 	//ATRIBUTOS
 	private int fila;
 	private char columna;
+	
+	//GET y SET
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) throws IllegalAccessException {
+		if(fila>8 || fila<1) {
+			throw new IllegalAccessException("El valor de fila no debe ser entre 1 y 8");
+		}
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) throws IllegalAccessException {
+		if(columna!='a' && columna!='b' && columna!='c' && columna!='d' && columna!='e' && columna!='f' && columna!='g' && columna!='h' 
+		   && columna!='A' && columna!='B' && columna!='C' && columna!='D' && columna!='E' && columna!='F' && columna!='G' && columna!='H') {
+			
+			throw new IllegalAccessException("La columna debe ser una caracter entre a y h");
+			
+		}
+		this.columna = columna;
+	}
+	
+	
 }
