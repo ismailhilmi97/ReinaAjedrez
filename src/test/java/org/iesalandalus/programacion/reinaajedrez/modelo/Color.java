0f3ddaf11a11a03207b36bcb73e9b59221bf8e2f
isma <isma@WINDELL-BI0390A>
2022-11-15 11:03:53 +0100

    Crear un enumerado color con dos literales
    Crear el atributo cadeneAMostrar
    Crear el constructor
    Crear el método toString

---
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -0,0 +1,20 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public enum Color {
+	
+	BLANCO("Blanco"),NEGRA("Negro");
+	
+	//ATRIBUTO
+	private String cadenaAMostrar;
+	
+	//CONSTRUCTOR
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	
+	//toSTRING
+	@Override
+    public String toString() {
+			return String.format("Posicion [cadenaAMostrar=%s]", cadenaAMostrar);
+		}
+}
