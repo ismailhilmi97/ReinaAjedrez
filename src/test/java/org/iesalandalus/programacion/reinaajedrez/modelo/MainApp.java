36752376f459ab022665a287ec623701c5568ea0
isma <isma@WINDELL-BI0390A>
2022-11-18 10:36:27 +0100

    Commit

--- src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/MainApp.java
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/MainApp.java
@@ -3,69 +3,68 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 import javax.naming.OperationNotSupportedException;
 
 public class MainApp {
-	private static  Reina reina;
-	//
-	
+	//ATRIBUTOS
+	private static Reina reina;
+
 	private static void ejecutarOpcion(int opcion) throws OperationNotSupportedException {
 		System.out.println("------- BIENVENIDO EN NUESTA APPLICACIÓN-------");
 		System.out.println();
 		Consola.elegirOpcionMenu();
 	}
-	
+
 	static void crearReinaDefecto() {
 		try {
 			System.out.println("*******Crear una reina por defecto*******");
 			System.out.println();
-			reina=new Reina();
-			System.out.println("Los informacion de la reina creada: "+reina.toString());
+			reina = new Reina();
+			System.out.println("Los informacion de la reina creada: " + reina.toString());
 			System.out.println();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 	}
-	
+
 	static void crearReinaColor() {
 		try {
 			System.out.println("*******Crear una reina elegiendo el color*******");
 			System.out.println();
 			reina = new Reina(Consola.elegirColor());
-			System.out.println("Informacio de la reina creada: "+reina.toString());
+			System.out.println("Informacio de la reina creada: " + reina.toString());
 			System.out.println(); // saltar de linea
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		 
+
 	}
-	
-	
+
 	static void mover() throws OperationNotSupportedException {
 		try {
 			System.out.println("*******Mover la reina*******");
-			Reina reina3 = new Reina(Consola.elegirColor());
-			System.out.println("Posicion Actual: " + reina3.toString());
+			reina = new Reina(Consola.elegirColor());
+			System.out.println("Posicion Actual: " + reina.toString());
 			System.out.println();
-			reina3.mover(Consola.elegirDireccion(), Consola.elegirPasos());
-			System.out.println("La nueva posicion: "+reina3.toString());
+			reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
+			System.out.println("La nueva posicion: " + reina.toString());
 			System.out.println();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	private void mostrarReina() {
 		try {
 			reina.toString();
 		} catch (Exception e) {
 			System.out.println("No se puede crear la reina.");
 		}
-		
+
 	}
-	
-	public static void main(String[] args) throws OperationNotSupportedException{
-		
+
+	public static void main(String[] args) throws OperationNotSupportedException {
+
 		ejecutarOpcion(1);
-		
+
 	}
 
 }
